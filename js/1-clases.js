92c697e5b22805e288086beab407edb0e61ee6be
Heidy <yulietholivera13@gmail.com>
2024-03-04 19:55:44 -0500

    creando proyecto

---
+++ js/1-clases.js
@@ -0,0 +1,11 @@
+// comentarios : Son lineas que el codigo no ejecuta (ignorado) y se usan normalmente para guiar
+ // a la perona que este interpretando el codigo
+
+
+// ´//´ sirve para una sola linea de codigo
+
+
+/***sirve para comentar
+varias lineas de codigo
+EL**/
+
